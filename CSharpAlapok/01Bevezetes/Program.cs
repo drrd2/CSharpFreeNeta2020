d73dff3b5c728a65d924fdf1c80bc0677ceebcef
RozDan - Neta <dr_rd2@vipmail.hu>
2020-05-03 14:13:16 +0200

    #2:  Hello world! 2

---
+++ CSharpAlapok/01Bevezetes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01Bevezetes
+{
+    //                        NETACADEMIA  -  Programozási alapismeretek C# nyelven (classic)
+
+
+    //    Program(C#)  ->  IL (Intermediate Language = Köztes nyelv)
+
+    //                      \ IL /      (.exe)
+    //                      ------
+    //                      |.NET|    (fordito)                      |.NET|                 |.NET|
+    //                      ------                                   ------                ------- 
+    //                      WINDOWS   (op.rendszer)                  LINUX                 Mac. OS           Android         iOS
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hali! - (Write)\n");
+            Console.WriteLine("Sziasztok, Itt vagyunk  - (WriteLine - új sort kezd)");
+            Console.WriteLine("Sziasztok, Itt vagyunk  - (WriteLine - új sort kezd)");
+            Console.ReadLine();   // ez fogja meg a console-t.    Bekér egy sortörést
+
+
+        }
+    }
+}
---
+++ CSharpAlapok/01HelloVilag_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01HelloVilag_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
