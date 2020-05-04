b9ddc440231682942880dd85d5d6fa91782754c6
drrd2 <dr_rd2@vipmail.hu>
2020-05-04 22:53:37 +0200

    #1: A C# nyelv Case Sensitive és a látszó karakterek fontosak

--- CSharpAlapok/01HelloVilag_2/Program.cs
+++ CSharpAlapok/01HelloVilag_2/Program.cs
@@ -6,6 +6,17 @@ namespace _01HelloVilag_2
     {
         static void Main(string[] args)
         {
+            // A C# Case sensitive nyelv, azaz mekölönbözteti a kis-Nagy betűket
+            // A consoloe nem ugyan az mint a Console
+
+            //Minden ami látszik (ami nem szóköz vagy enter), annak fontos jelentése van. Nem elhagyható!
+
+            // Pl.: nem mindegy:
+            /*
+             *"A királynét megölni nem kell, félnetek jó lesz "          nem ugyan az mint
+             * "A királynőt megölni, nem kell félnetek, jó lesz" 
+             */
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
