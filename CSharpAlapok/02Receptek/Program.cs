ebf6e0565a911eb6745f7acf3ea0f1e14b1f3072
drrd2 <dr_rd2@vipmail.hu>
2020-05-05 19:07:24 +0200

    #2: Egy recept kiírása és enterre várakozás

---
+++ CSharpAlapok/02Receptek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02Receptek
+{
+    class Program
+    {
+        /// <summary>
+        /// Legyen az a feladat, hogy egy pörkölt recept hozzávalóit kiírjuk a képernyőre
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Marhahús");
+            Console.WriteLine("Vöröshagyma");
+            Console.WriteLine("Olaj");
+            Console.WriteLine("Piros paprika");
+
+            Console.ReadLine();
+        }
+    }
+}
